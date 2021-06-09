639a4f6a2566fb8206763d29d64f4682c90f3d40
Kanha Agrawal <kanha.agr11@gmail.com>
2021-06-09 18:21:43 +0530

    fixed logout issues

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = require('express-promise-router')();
 router.route('/adminLogin').post(adminAuth.login)
 router.route('/adminSignUp').post(adminAuth.signUp)
 //render the web site or forntend
-router.route('/').get((req,res)=>{res.render('agrawalMedical')})
+router.route('/').get((req, res) => { res.render('agrawalMedical') })
 //clear the cookie
-router.route('/logOut').post((req,res)=>{res.clearCookie('meriPechan');res.status(200).send("done")})    
+router.route('/logOut').post((req, res) => { res.clearCookie(process.env.TOKEN_NAME); res.status(200).send("done") })
 module.exports = router;
