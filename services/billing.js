abe59d887208bd19012036ee19484ade766d174e
kanha <kanha.agr11@gmail.com>
2023-08-19 00:20:01 +0530

    changed invoice number sequence

--- services/billing.js
+++ services/billing.js
@@ -1,10 +1,16 @@
 const Bill = require("../models/billing")
 const CN = require("../models/creditNote")
 
+const generatebillnumber = (billno) => {
+  const char = String.fromCharCode(65 + (parseInt(billno / 999999)))
+  let result = (billno % 999999).toString().padStart(6, '0')
+  return char + result;
+}
+
 const addBill = async (data) => {
   try {
     const invoiceCount = await Bill.countDocuments()
-    data.billInfo.invoiceNo = invoiceCount + 1
+    data.billInfo.invoiceNo = generatebillnumber(invoiceCount + 1)
     data.billInfo.patientName = data.billInfo.patientName.toUpperCase()
     if (data.billInfo.prescribedBy)
       data.billInfo.prescribedBy = data.billInfo.prescribedBy.toUpperCase()
